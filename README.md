ce49e48d431084bd1ad090ce6bb40c44c6211e05
AdrianoRAraujo365 <adriano.unifesp2012@gmail.com>
2026-09-18 08:19:34 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# spring-boot-api-exemplo-2
+# spring-boot-api-exemplos
+Exemplos didáticos de APIs REST desenvolvidas com Java e Spring Boot para as disciplinas do curso DSM
---
+++ src/main/java/br/com/fatec/apiexemplousuario/model/Usuario.java
@@ -0,0 +1,44 @@
+package br.com.fatec.apiexemplousuario.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private int idade;
+
+
+    public Usuario() {}
+
+    public Usuario(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+
+    }
+
+    public Integer getId() {return id;}
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
+
+
+
+
